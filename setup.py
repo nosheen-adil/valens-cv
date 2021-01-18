02276cb10524ceeda6bceaa58f18fe939e27f1f7
Nosheen Adil <nosheen.0398@gmail.com>
2021-01-18 13:59:04 -0500

    setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='valens',
+    version='0.0.1',
+    description='Computer vision module for valens',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
