80ef5aed0661ac10d9425f24954cf15bf8147360
Nosheen Adil <nosheen.0398@gmail.com>
2021-02-16 00:50:19 -0500

    add pose constants

--- valens/constants.py
+++ valens/constants.py
@@ -6,3 +6,5 @@ TEST_DATA_DIR = PYTHON_DIR + "/test/data"
 
 POSE_MODEL_WIDTH = 224
 POSE_MODEL_HEIGHT = 224
+POSE_MODEL_TRT_WEIGHTS = DATA_DIR + "/resnet18_baseline_att_224x224_A_epoch_249_trt.pth"
+POSE_JSON = DATA_DIR + "/human_pose.json"
