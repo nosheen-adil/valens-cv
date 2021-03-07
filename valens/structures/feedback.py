ef04e3d286d5e8d7533ea6a758af6e8a46e3e0be
Nosheen Adil <nosheen.0398@gmail.com>
2021-03-07 10:57:24 -0500

    draw feedback on image

--- valens/structures/feedback.py
+++ valens/structures/feedback.py
@@ -1,5 +1,7 @@
+from valens import constants
 from valens.structures.pose import Keypoints
 import numpy as np
+import cv2
 
 def to_json(user_pose, corrected_pose=None, keypoints=[]):
     if corrected_pose is not None:
@@ -34,3 +36,88 @@ def to_json(user_pose, corrected_pose=None, keypoints=[]):
             }
 
     return result
+
+def topology(topology, keypoints):
+    result = []
+    for link in topology:
+        k1, k2 = link
+        result.append([str(keypoints[k1]), str(keypoints[k2])])
+    return result
+    
+
+def scale(feedback, width=constants.POSE_MODEL_WIDTH, height=constants.POSE_MODEL_HEIGHT):
+    result = feedback.copy()
+    for keypoint in result['pose'].keys():
+        if result['pose'][keypoint]['x'] == np.nan:
+            result['pose'][keypoint]['x'] = -1
+        else:
+            result['pose'][keypoint]['x'] = round(result['pose'][keypoint]['x'] * width)
+
+        if result['pose'][keypoint]['y'] == np.nan:
+            result['pose'][keypoint]['y'] = -1
+        else:
+            result['pose'][keypoint]['y'] = round(result['pose'][keypoint]['y'] * height)
+
+
+    for keypoint in result['feedback'].keys():
+        if not result['feedback'][keypoint]['correct']:
+            result['feedback'][keypoint]['x'] = round(result['feedback'][keypoint]['x'] * width)
+            result['feedback'][keypoint]['y'] = round(result['feedback'][keypoint]['y'] * height)
+
+    return result
+
+def draw_on_image(feedback, frame, topology, user_colors={'good' : (0, 255, 0), 'bad' : (0, 0, 255)}, feedback_color=(255, 255, 255)):
+    print(feedback)
+    result = scale(feedback, frame.shape[0], frame.shape[1])
+
+    for keypoint in result['pose'].keys():
+        x = result['pose'][keypoint]['x']
+        y = result['pose'][keypoint]['y']
+
+        if x >= 0 and y >= 0:
+            correct = result['feedback'][keypoint]['correct']
+            
+            if correct:
+                cv2.circle(frame, (x, y), 3, user_colors['good'], 2)
+            else:
+                f_x = result['feedback'][keypoint]['x']
+                f_y = result['feedback'][keypoint]['y']
+                cv2.circle(frame, (x, y), 3, user_colors['bad'], 2)
+                cv2.circle(frame, (f_x, f_y), 3, feedback_color, 2)
+    print(result)
+    for link in topology:
+        keypoint1, keypoint2 = link
+
+        x1 = result['pose'][keypoint1]['x']
+        y1 = result['pose'][keypoint1]['y']
+        x2 = result['pose'][keypoint2]['x']
+        y2 = result['pose'][keypoint2]['y']
+
+        if x1 >= 0 and y1 >= 0 and x2 >= 0 and y2 >= 0:
+            correct1 = result['feedback'][keypoint1]['correct']
+            correct2 = result['feedback'][keypoint2]['correct']
+
+            print(keypoint1, correct1, keypoint2, correct2)
+
+            if correct1 and correct2:
+                cv2.line(frame, (x1, y1), (x2, y2), user_colors['good'], 2)
+            else:
+                cv2.line(frame, (x1, y1), (x2, y2), user_colors['bad'], 2)
+            
+                if correct1 and not correct2:
+                    f_x2 = result['feedback'][keypoint2]['x']
+                    f_y2 = result['feedback'][keypoint2]['y']
+                    cv2.line(frame, (x1, y1), (f_x2, f_y2), feedback_color, 2)
+                elif not correct1 and correct2:
+                    f_x1 = result['feedback'][keypoint1]['x']
+                    f_y1 = result['feedback'][keypoint1]['y']
+                    cv2.line(frame, (f_x1, f_y1), (x2, y2), feedback_color, 2)
+                else:
+                    f_x2 = result['feedback'][keypoint2]['x']
+                    f_y2 = result['feedback'][keypoint2]['y']
+
+                    f_x1 = result['feedback'][keypoint1]['x']
+                    f_y1 = result['feedback'][keypoint1]['y']
+                    print(f_x1, f_y1, f_x2, f_y2)
+                    cv2.line(frame, (f_x1, f_y1), (f_x2, f_y2), feedback_color, 2)
+                    
--- valens/test/structures/test_feedback.py
+++ valens/test/structures/test_feedback.py
@@ -4,6 +4,8 @@ import valens.structures.feedback
 from valens.structures.pose import Keypoints
 
 import numpy as np
+import cv2
+import h5py
 
 def test_feedback_to_json_correct():
     keypoints = [Keypoints.RANKLE, Keypoints.RKNEE, Keypoints.RHIP, Keypoints.NECK]
@@ -35,7 +37,7 @@ def test_feedback_to_json_incorrect():
     expected_pose[3, 1] += abs(pose[2, 1] - pose[1, 1])
     expected_pose[3, 0] = expected_pose[2, 0]
 
-    result = core.feedback.to_json(pose, expected_pose, keypoints)
+    result = core.feedback.to_json(pose, corrected_pose=expected_pose, keypoints=keypoints)
     for i, keypoint in [[0, 'right_ankle'], [1, 'right_knee']]:
         assert result['pose'][keypoint]['x'] == pose[i, 0]
         assert result['pose'][keypoint]['y'] == pose[i, 1]
@@ -47,4 +49,128 @@ def test_feedback_to_json_incorrect():
         assert result['feedback'][keypoint]['correct'] == False
         assert result['feedback'][keypoint]['x'] == expected_pose[i, 0]
         assert result['feedback'][keypoint]['y'] == expected_pose[i, 1]
+
+def test_feedback_topology():
+    keypoints = [Keypoints.RANKLE, Keypoints.RKNEE, Keypoints.RHIP, Keypoints.NECK]
+    topology = [[0, 1], [1, 2], [2, 3]] # [ankle -> knee, knee -> hip, hip -> neck]
+    feedback_topology = core.feedback.topology(topology, keypoints)
+    assert feedback_topology == [['right_ankle', 'right_knee'], ['right_knee', 'right_hip'], ['right_hip', 'neck']]
+
+def test_feedback_draw_on_image_correct():
+    keypoints = [Keypoints.RANKLE, Keypoints.RKNEE, Keypoints.RHIP, Keypoints.NECK]
+    topology = [[0, 1], [1, 2], [2, 3]] # [ankle -> knee, knee -> hip, hip -> neck]
+    feedback_topology = core.feedback.topology(topology, keypoints)
+
+    pose = np.array([
+        [0.5, 0.7],
+        [0.5, 0.5],
+        [0.5, 0.35],
+        [0.5, 0.1]
+    ])
+    feedback = {
+        'pose' : {
+            'right_ankle' : {
+                'x' : pose[0, 0],
+                'y' : pose[0, 1]
+            },
+            'right_knee' : {
+                'x' : pose[1, 0],
+                'y' : pose[1, 1]
+            },
+            'right_hip' : {
+                'x' : pose[2, 0],
+                'y' : pose[2, 1]
+            },
+            'neck' : {
+                'x' : pose[3, 0],
+                'y' : pose[3, 1]
+            }
+        },
+        'feedback' : {
+            'right_ankle' : {
+                'correct' : True
+            },
+            'right_knee' : {
+                'correct' : True
+            },
+            'right_hip' : {
+                'correct' : True
+            },
+            'neck' : {
+                'correct' : True
+            }
+        }
+    }
+
+    expected_frame = np.zeros((constants.POSE_MODEL_WIDTH, constants.POSE_MODEL_HEIGHT, 3), dtype=np.uint8)
+    core.pose.draw_on_image(pose, expected_frame, topology)
+
+    actual_frame = np.zeros((constants.POSE_MODEL_WIDTH, constants.POSE_MODEL_HEIGHT, 3), dtype=np.uint8)
+    core.feedback.draw_on_image(feedback, actual_frame, feedback_topology)
+
+    np.testing.assert_equal(actual_frame, expected_frame)
+
+def test_feedback_draw_on_image_incorrect():
+    keypoints = [Keypoints.RANKLE, Keypoints.RKNEE, Keypoints.RHIP, Keypoints.NECK]
+    topology = [[0, 1], [1, 2], [2, 3]] # [ankle -> knee, knee -> hip, hip -> neck]
+    feedback_topology = core.feedback.topology(topology, keypoints)
+
+    pose = np.array([
+        [0.5, 0.7],
+        [0.5, 0.5],
+        [0.5, 0.35],
+        [0.5, 0.1]
+    ])
+    expected_pose = pose.copy()
+    expected_pose[2, 0] -= abs(pose[2, 1] - pose[1, 1])
+    expected_pose[2, 1] = pose[1, 1]
+    expected_pose[3, 1] += abs(pose[2, 1] - pose[1, 1])
+    expected_pose[3, 0] = expected_pose[2, 0]
+
+    feedback = {
+        'pose' : {
+            'right_ankle' : {
+                'x' : pose[0, 0],
+                'y' : pose[0, 1]
+            },
+            'right_knee' : {
+                'x' : pose[1, 0],
+                'y' : pose[1, 1]
+            },
+            'right_hip' : {
+                'x' : pose[2, 0],
+                'y' : pose[2, 1]
+            },
+            'neck' : {
+                'x' : pose[3, 0],
+                'y' : pose[3, 1]
+            }
+        },
+        'feedback' : {
+            'right_ankle' : {
+                'correct' : True
+            },
+            'right_knee' : {
+                'correct' : True
+            },
+            'right_hip' : {
+                'correct' : False,
+                'x' : expected_pose[2, 0],
+                'y' : expected_pose[2, 1]
+            },
+            'neck' : {
+                'correct' : False,
+                'x' : expected_pose[3, 0],
+                'y' : expected_pose[3, 1]
+            }
+        }
+    }
+
+    actual_frame = np.zeros((constants.POSE_MODEL_WIDTH, constants.POSE_MODEL_HEIGHT, 3), dtype=np.uint8)
+    core.feedback.draw_on_image(feedback, actual_frame, feedback_topology)
+
+    expected_filename = constants.TEST_DATA_DIR + '/feedback_draw_on_image_incorrect.h5'
+    with h5py.File(expected_filename, 'r') as data:
+        expected_frame = data['image'][:]
     
+    np.testing.assert_equal(actual_frame, expected_frame)
